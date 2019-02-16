dae36919ebc94495482780e71f651566e534fe15
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-02-15 17:13:04 -0700

    Changed Arrival and Departure date inputs to TouchableOpacity

--- src/app.style.tsx
+++ src/app.style.tsx
@@ -1,73 +1,88 @@
-import * as React from 'react';
-import {
-	StyleSheet
-} from "react-native";
-
-const $TextInput = {
-	height: 40,
-	width: '100%',
-	borderColor: 'gainsboro',
-	borderWidth: 2,
-	marginTop: 12,
-	padding: 4
-};
+import * as React from "react";
+import { StyleSheet } from "react-native";
 
 const $ContainerBase = {
-	flex: 1,
-	justifyContent: 'center',
-	alignItems: "center",
-	backgroundColor: "#F5FCFF",
-	padding: 12
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "#F5FCFF",
+  padding: 12
+};
+
+const $TextInput = {
+  paddingLeft: 8,
+  height: 40,
+  width: "100%",
+  borderColor: "gainsboro",
+  borderWidth: 2,
+  marginTop: 12,
+  padding: 4
+};
+
+const $DateInput = {
+  paddingLeft: 8,
+  paddingTop: 9,
+  height: 40,
+  width: "100%",
+  borderColor: "orange",
+  borderWidth: 2,
+  padding: 4
 };
 
 const AppStyle = StyleSheet.create({
-	btn: {
-		marginBottom: 12
-	},
-	btnMakeReservation: {
-		marginTop: 200,
-		color: 'red'
-	},
-	arrivalView: {
-		height: 40,
-		width: '100%'
-	},
-	arrivalDate: {
-		...$TextInput,
-		borderColor: 'blue'
-	},
-	departureDate: {
-		...$TextInput,
-		borderColor: 'hotpink'
-	},
-	firstName: {
-		...$TextInput,
-		borderColor: 'green'
-	},
-	lastName: {
-		...$TextInput,
-		borderColor: 'red',
-		marginBottom: 24
-	},
-	displayText: {
-		marginBottom: 4
-	},
-	container: {
-		...$ContainerBase
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: "center",
-		margin: 10
-	},
-	instructions: {
-		textAlign: "center",
-		color: "red",
-		marginBottom: 5
-	},
-	input: {
-		color: 'blue'
-	}
+  placeholderText: {
+    color: "gainsboro"
+  },
+  dateSelectionTouchable: {
+    height: 40,
+    width: "100%",
+    marginTop: 12
+  },
+  arrivalDate: {
+    ...$DateInput
+  },
+  departureDate: {
+    ...$DateInput
+  },
+  btn: {
+    marginBottom: 12
+  },
+  btnMakeReservation: {
+    marginTop: 200,
+    color: "red"
+  },
+  arrivalView: {
+    height: 40,
+    width: "100%"
+  },
+  firstName: {
+    ...$TextInput,
+    borderColor: "green"
+  },
+  lastName: {
+    ...$TextInput,
+    borderColor: "red",
+    marginBottom: 24
+  },
+  displayText: {
+    marginBottom: 4
+  },
+  container: {
+    ...$ContainerBase
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "red",
+    marginBottom: 5
+  },
+  input: {
+    color: "blue"
+  }
 });
 
-export {AppStyle}
+export { AppStyle };
--- src/app.tsx
+++ src/app.tsx
@@ -1,171 +1,211 @@
-import * as React from 'react';
+import * as React from "react";
 import {
-    Platform,
-    Text,
-    View,
-    TextInput,
-	TouchableOpacity
+  Platform,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity
 } from "react-native";
 
-import DateTimePicker from 'react-native-modal-datetime-picker';
+import DateTimePicker from "react-native-modal-datetime-picker";
 
-import _ from 'lodash';
+import _ from "lodash";
 
-import { Button } from 'react-native-elements';
+import { Button } from "react-native-elements";
 
 import { AppStyle } from "./app.style";
 
 const instructions = Platform.select({
-    ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-    android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu"
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
-
-const $Divider = {
-	color: 'gainsboro'
+type State = {
+  arrivalDate: string;
+  departureDate: string;
+  hotel: string;
+  firstName: string;
+  lastName: string;
+  name: string;
+  reservationId: string;
+  isDateTimePickerVisible: boolean;
+  dateType: any;
 };
+
+function formatDate(inputFormat: Date) {
+  function pad(s: any) {
+    return s < 10 ? "0" + s : s;
+  }
+
+  let d = new Date(inputFormat);
+
+  return [pad(d.getMonth() + 1), pad(d.getDate()), d.getFullYear()].join("/");
+}
 
 let $PlaceholderOpacity = 0.7;
 
 class App extends React.PureComponent<Props, State> {
-	constructor(props: any) {
-		super(props);
+  constructor(props: any) {
+    super(props);
 
-		this.state = {
-			firstName: '',
-			lastName: '',
-			text: '',
-			location: '',
-			arrivalDate: '',
-			departureDate: '',
-			hotel: '',
-			name: '',
+    this.state = {
+      arrivalDate: "",
+      departureDate: "",
+      hotel: "",
+      firstName: "",
+      lastName: "",
+      name: "",
+      reservationId: "",
+      isDateTimePickerVisible: false,
+      dateType: null
+    };
+  }
 
-			isDateTimePickerVisible: false,
-			dateType: null
-
-		};
-	}
-
-	private _showDateTimePicker = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, type: string): void => {
-		if (type === 'arrival') {
-			this.setState({
-				isDateTimePickerVisible: true,
-				dateType: 'arrival'
-			});
-		} else if (type === 'departure') {
-			this.setState({
-				isDateTimePickerVisible: true,
-				dateType: 'departure'
-			});
-		}
-	};
-
-	private _onChangeText = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, text: string): void => {
-
-	};
-
-	_hideDateTimePicker () {
-		this.setState({
-			isDateTimePickerVisible: false,
-			dateType: null,
-		});
-	}
-
-	_handleDatePicked (date: any) {
-		console.log('A date has been picked: ', date);
-		if (this.state.dateType === 'arrival') {
-			this.setState({
-				arrivalDate: date,
-				isDateTimePickerVisible: false,
-				dateType: null,
-			})
-		} else if (this.state.dateType === 'departure') {
-			this.setState({
-				departureDate: date,
-				isDateTimePickerVisible: false,
-				dateType: null,
-			})
-		}
-	}
-
-	_createReservation () {
-
-		if (this.state.firstName.length > 0 && this.state.lastName.length > 0) {
-			let name = `${this.state.firstName} ${this.state.lastName}`;
-			this.setState({
-				name: name
-			})
-		}
-
+  private _showDateTimePicker = (type: string): void => {
+    if (type === "arrival") {
+      this.setState({
+        isDateTimePickerVisible: true,
+        dateType: "arrival"
+      });
+    } else if (type === "departure") {
+      this.setState({
+        isDateTimePickerVisible: true,
+        dateType: "departure"
+      });
     }
+  };
 
-    render() {
-        return (
-            <View style={AppStyle.container}>
+  private _hideDateTimePicker = (): void => {
+    this.setState({
+      isDateTimePickerVisible: false,
+      dateType: null
+    });
+  };
 
-				<DateTimePicker
-					isVisible={this.state.isDateTimePickerVisible}
-					onConfirm={this._handleDatePicked.bind(this)}
-					onCancel={this._hideDateTimePicker.bind(this)}
-				/>
-
-				<TextInput
-					id={'arrivalDate'}
-					style={[AppStyle.arrivalDate, {opacity: this.state.firstName.length ? 1 : $PlaceholderOpacity}]}
-					placeholder="Arrival date"
-					placeholderTextColor="#ccc"
-					onFocus={(e) => this._showDateTimePicker(e, 'arrival')}
-					value={this.state.arrivalDate}
-				/>
-
-				<TextInput
-					id={'firstNameInput'}
-					style={[AppStyle.firstName, {opacity: this.state.firstName.length ? 1 : $PlaceholderOpacity}]}
-					placeholder="First name"
-					placeholderTextColor="#ccc"
-					onChangeText={(text) => {
-						let name = _.toLower(text);
-						this.setState({
-							firstName: _.upperFirst(name)
-						});
-					}}
-					value={this.state.firstName}
-				/>
-
-				<TextInput
-					id={'lastNameInput'}
-					style={[AppStyle.lastName, {opacity: this.state.lastName.length ? 1 : $PlaceholderOpacity}]}
-					placeholder="Last name"
-					placeholderTextColor="#ccc"
-					onChangeText={(text) => {
-						let name = _.toLower(text);
-						this.setState({
-							lastName: _.upperFirst(name)
-						});
-					}}
-
-					value={this.state.lastName}
-				/>
-
-
-				<Button
-					title={'Create Reservation'}
-					type={'outline'}
-					style={AppStyle.btn}
-					disabled={false}
-					onPress={this._createReservation.bind(this)}
-				/>
-
-                <Text style={AppStyle.welcome}>{this.state.name}</Text>
-
-
-
-
-            </View>
-        );
+  private _handleDatePicked = (date: Date): void => {
+    if (this.state.dateType === "arrival") {
+      //TODO: CHECK DATE SEQUENCE
+      let arrival = formatDate(date);
+      this.setState({
+        arrivalDate: arrival,
+        isDateTimePickerVisible: false,
+        dateType: null
+      });
+    } else if (this.state.dateType === "departure") {
+      //TODO: CHECK DATE SEQUENCE
+      let departure = formatDate(date);
+      this.setState({
+        departureDate: departure,
+        isDateTimePickerVisible: false,
+        dateType: null
+      });
     }
+  };
+
+  private _createReservation = (): void => {
+    if (this.state.firstName.length > 0 && this.state.lastName.length > 0) {
+      let name = `${this.state.firstName} ${this.state.lastName}`;
+      this.setState({
+        name: name
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View style={AppStyle.container}>
+        <DateTimePicker
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this._handleDatePicked}
+          onCancel={() => {
+            this._hideDateTimePicker();
+          }}
+        />
+
+        <TouchableOpacity
+          style={AppStyle.dateSelectionTouchable}
+          onPress={() => {
+            this._showDateTimePicker("arrival");
+          }}
+        >
+          <Text style={AppStyle.arrivalDate}>
+            {this.state.arrivalDate.length > 0 ? (
+              this.state.arrivalDate
+            ) : (
+              <Text style={AppStyle.placeholderText}>Arrival date</Text>
+            )}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={AppStyle.dateSelectionTouchable}
+          onPress={() => {
+            this._showDateTimePicker("departure");
+          }}
+        >
+          <Text style={AppStyle.departureDate}>
+            {this.state.departureDate.length > 0 ? (
+              this.state.departureDate
+            ) : (
+              <Text style={AppStyle.placeholderText}>Departure date</Text>
+            )}
+          </Text>
+        </TouchableOpacity>
+
+        <TextInput
+          ref={"firstNameTextInput"}
+          style={[
+            AppStyle.firstName,
+            { opacity: this.state.firstName.length ? 1 : $PlaceholderOpacity }
+          ]}
+          placeholder="First name"
+          placeholderTextColor="#ccc"
+          onChangeText={(text: string) => {
+            let name = _.toLower(text);
+            this.setState({
+              firstName: _.upperFirst(name)
+            });
+          }}
+          value={this.state.firstName}
+        />
+
+        <TextInput
+          ref={"lastNameTextInput"}
+          style={[
+            AppStyle.lastName,
+            { opacity: this.state.lastName.length ? 1 : $PlaceholderOpacity }
+          ]}
+          placeholder="Last name"
+          placeholderTextColor="#ccc"
+          onChangeText={(text: string) => {
+            let name = _.toLower(text);
+            this.setState({
+              lastName: _.upperFirst(name)
+            });
+          }}
+          value={this.state.lastName}
+        />
+
+        <Button
+          title={"Create Reservation"}
+          type={"outline"}
+          style={AppStyle.btn}
+          onPress={this._createReservation}
+        />
+
+        {this.state.name.length > 0 ? (
+          <View>
+            <Text style={AppStyle.welcome}>{this.state.name}</Text>
+            <Text style={AppStyle.welcome}>{this.state.departureDate}</Text>
+            <Text style={AppStyle.welcome}>{this.state.arrivalDate}</Text>
+          </View>
+        ) : null}
+      </View>
+    );
+  }
 }
 
 export default App;
