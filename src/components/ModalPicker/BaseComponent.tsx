48d62a5e224c595c14a23fa03909baf4ffb87978
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-02-19 10:51:13 -0700

    Removed unused files.

--- src/components/ModalPicker/BaseComponent.tsx
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import React from 'react';
-
-export default class BaseComponent extends React.Component {
-    _bind(...methods: any) {
-        methods.forEach( (method: any) => this[method: any] = this[method].bind(this) );
-    }
-}
