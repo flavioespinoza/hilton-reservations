996468c1c7ab254a5d4556586d5f75563e7e184b
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-02-21 11:47:06 -0700

    Chaning clientState to work more betta!!!  ;)

--- AppLinkState.tsx
+++ AppLinkState.tsx
@@ -6,7 +6,7 @@ import { ApolloProvider, Query, graphql } from 'react-apollo'
 import { _QueryReservations, _MutationCreateReservation } from './api/query'
 import _ from 'lodash'
 
-const TypeMutation = `
+const TypeMutation =`
     type Mutation {
             createReservation(data: ReservationCreateInput!): Reservation!
             updateReservation(data: ReservationUpdateInput!, where: ReservationWhereUniqueInput!): Reservation
@@ -47,50 +47,45 @@ const TypeQuery = `
 `
 const cache = new InMemoryCache()
 
-const clientState = {
-    defaults: {
-        isConnected: true
-    },
-    resolvers: {
-        Query: {
-            reservations: () => {
-                return _QueryReservations()
-            }
-        },
-        Mutation: {
-            createReservation: async (obj: any) => {
-                const bookReservation = await _MutationCreateReservation(
-                    obj.name,
-                    obj.hotelName,
-                    obj.arrivalDate,
-                    obj.departureDate
-                )
-                const timestamp = _.now()
-                const id = `create_reservation_${timestamp}`
-                if (bookReservation) {
-                    cache.writeData({
-                        id: id,
-                        data: bookReservation
-                    })
-                } else {
-                    const error_id = `error_create_reservation_${timestamp}`
-                    cache.writeData({
-                        id: error_id,
-                        data: bookReservation
-                    })
-                }
-            }
-        }
-    },
-    typeDefs: {
-        Mutation: TypeMutation,
-        Query: TypeQuery
-    }
-}
-
 const client = new ApolloClient({
     uri: 'https://us1.prisma.sh/public-luckox-377/reservation-graphql-backend/dev',
-    cache: cache
+    cache: cache,
+    clientState: {
+        defaults: {
+            isConnected: true
+        },
+        resolvers: {
+            Query: {
+                reservations: () => {
+                    return _QueryReservations()
+                }
+            },
+            Mutation: {
+                createReservation: async (obj: any) => {
+                    const bookReservation = await _MutationCreateReservation(obj.name, obj.hotelName, obj.arrivalDate, obj.departureDate)
+                    const timestamp = _.now()
+                    const id = `create_reservation_${timestamp}`
+                    if (bookReservation) {
+                        cache.writeData({ 
+                            id: id, 
+                            data: bookReservation 
+                        })
+                    } else {
+                        const error_id = `error_create_reservation_${timestamp}`
+                        cache.writeData({ 
+                            id: error_id, 
+                            data: bookReservation 
+                        })
+
+                    }
+                }
+            }
+        },
+        typeDefs: {
+            Mutation: TypeMutation,
+            Query: TypeQuery
+        }
+    }
 })
 
 class App extends React.Component {
