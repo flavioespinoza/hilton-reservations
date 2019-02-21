38bd11d4034ce48f16c5264fd0296ac52b146ee8
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-02-20 22:58:35 -0700

    Setup for client side state management with Appolo

---
+++ src/app.options.tsx
@@ -0,0 +1,27 @@
+const TabBarOptions = {
+    tabBarOptions: {
+        activeTintColor: '#EF5356',
+        allowFontScaling: true,
+        inactiveTintColor: 'gray',
+        pressColor: '#EF5356',
+        pressOpacity: 0.5,
+        showIcon: false,
+        showLabel: true,
+        upperCaseLabel: true,
+        indicatorStyle: {
+            backgroundColor: '#EF5356',
+            opacity: 100
+        },
+        activeTabStyle: {
+            backgroundColor: 'red'
+        },
+        style: {
+            backgroundColor: 'white'
+        }
+    },
+    animationEnabled: true,
+    tabBarVisible: true,
+    swipeEnabled: true
+}
+
+export { TabBarOptions }
--- src/app.tsx
+++ src/app.tsx
@@ -4,44 +4,24 @@ import CreateReservation from './screens/CreateReservation/CreateReservation'
 import Reservations from './screens/Reservations/Reservations'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import { TabBarOptions } from './app.options'
 
 // Apollo client
 const client = new ApolloClient({
-    uri: 'https://us1.prisma.sh/public-luckox-377/reservation-graphql-backend/dev'
+    uri: 'https://us1.prisma.sh/public-luckox-377/reservation-graphql-backend/dev',
+    clientState: {
+        defaults: {},
+        resolvers: {},
+        typeDefs: {}
+    }
 })
-
-const tabBarOptions = {
-    tabBarOptions: {
-        activeTintColor: '#EF5356',
-        allowFontScaling: true,
-        inactiveTintColor: 'gray',
-        pressColor: '#EF5356',
-        pressOpacity: 0.5,
-        showIcon: false,
-        showLabel: true,
-        upperCaseLabel: true,
-        indicatorStyle: {
-            backgroundColor: '#EF5356',
-            opacity: 100
-        },
-        activeTabStyle: {
-            backgroundColor: 'red'
-        },
-        style: {
-            backgroundColor: 'white'
-        }
-    },
-    animationEnabled: true,
-    tabBarVisible: true,
-    swipeEnabled: true
-}
 
 const Tabs = TabNavigator(
     {
         ['Reservations']: { screen: Reservations },
         ['Create Reservation']: { screen: CreateReservation }
     },
-    tabBarOptions
+    TabBarOptions
 )
 
 class App extends React.Component {
