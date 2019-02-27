d9c2ca865dee4f010aa9e68c90c06db187ad1a5b
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-02-27 15:34:09 -0700

    Added deep-equal checks for graphql query and mutation template-literals

--- src/api/mutation.spec.js
+++ src/api/mutation.spec.js
@@ -8,7 +8,7 @@ const INPUT = {
     departureDate: '2/1/1'
 }
 
-const CHECK_MUTATION_CREATE_RESERVATION = obj => {
+const CHECK_MUTATION_CREATE_RESERVATION = () => {
     return `
         mutation {
             createReservation(
@@ -30,9 +30,9 @@ const CHECK_MUTATION_CREATE_RESERVATION = obj => {
     `
 }
 
-describe('mutation create reservation', function() {
+describe('mutation create reservation', () => {
     
-    it ('should deep equal CHECK_MUTATION_CREATE_RESERVATION', async function (done) {
+    it ('should deep equal CHECK_MUTATION_CREATE_RESERVATION', async (done) => {
         let mutation_create_reservation = await _MutationCreateReservation(INPUT.name, INPUT.hotelName, INPUT.arrivalDate, INPUT.departureDate)
         deepEqual(CHECK_MUTATION_CREATE_RESERVATION(), mutation_create_reservation)
         done()
